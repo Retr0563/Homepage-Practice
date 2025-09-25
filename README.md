0cbe8465a362bb8e0d9ac2c5c80f95149de1a577
Retr0 <nanayawbee2014@gmail.com>
2025-09-25 10:13:06 +0000

    Homepage Practice

---
+++ README.md
@@ -0,0 +1,5 @@
+# Creating my own webpage without any tutorials
+
+Just assessing how much i Have learnt and can produce on my own
+
+Hopefully I don't crash out and quit
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Sidebar from "./Components/Sidebar/Sidebar";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Sidebar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
